ad56438c185e96d3fa9362fb7a19b72f6fe6c008
Admin <landrzejewski.poczta@gmail.com>
2025-07-01 16:59:58 +0200

    update

--- setup/setup.sh
+++ setup/setup.sh
@@ -84,8 +84,8 @@ systemctl enable fail2ban
 systemctl restart fail2ban
 
 # === Install Docker ===
-sudo apt-get update
-sudo apt-get install ca-certificates curl
+sudo apt-get update -y
+sudo apt-get install ca-certificates curl -y
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
@@ -96,7 +96,7 @@ echo \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
 usermod -aG docker "$USER_NAME"
 
